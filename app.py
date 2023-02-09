ef79c1c295c8ba916ca62b740caf9335f5ac86c2
Christens <christens@viken.no>
2023-02-09 11:22:26 +0100

    fikset feil, json filene kan nå vises på index

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 from jsonapp import json_filmer
+filmer = json_filmer()
 
 @app.route("/")
 def index():
-    return render_template("index.html", filmer=json_filmer())
+    return render_template("index.html", filmer=filmer)
--- jsonapp.py
+++ jsonapp.py
@@ -1,7 +1,7 @@
 import json
 
 def json_filmer():
-    fil = open("imdb.json")
+    fil = open("json/imdb.json")
     filmer = json.load(fil)
     fil.close()
     return filmer
