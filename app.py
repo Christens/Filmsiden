c7f7dc14170775e4a5bcf0c19109f0a2ab3e3b2a
Christens <christens@viken.no>
2023-02-09 11:14:31 +0100

    tester at json-filen fungerer

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-from json import json_filmer
+from jsonapp import json_filmer
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", filmer=json_filmer())
